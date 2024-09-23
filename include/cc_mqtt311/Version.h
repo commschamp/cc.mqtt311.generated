63b3810397f4dc661b831a45fc998f45bbc794ff
Alex Robenko <arobenko@gmail.com>
2024-09-23 11:24:50 +1000

    Using v2.9 of cc.mqtt311.commsdsl to generate current code.

--- include/cc_mqtt311/Version.h
+++ include/cc_mqtt311/Version.h
@@ -14,10 +14,10 @@
 #define CC_MQTT311_MAJOR_VERSION (2)
 
 /// @brief Minor version of the protocol library.
-#define CC_MQTT311_MINOR_VERSION (8)
+#define CC_MQTT311_MINOR_VERSION (9)
 
 /// @brief Patch version of the protocol library.
-#define CC_MQTT311_PATCH_VERSION (1)
+#define CC_MQTT311_PATCH_VERSION (0)
 
 /// @brief Full version of the protocol library as single number.
 #define CC_MQTT311_VERSION (COMMS_MAKE_VERSION(CC_MQTT311_MAJOR_VERSION, CC_MQTT311_MINOR_VERSION, CC_MQTT311_PATCH_VERSION))
